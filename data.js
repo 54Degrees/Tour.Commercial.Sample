91d628f2410b2c2dc6dd9b343a69bec38390bf6c
Ellen Corcoran-Walsh <54degreesdesign@gmail.com>
2017-03-21 21:28:05 +0000

    Commercial Tour (SAMPLE)

---
+++ data.js
@@ -0,0 +1,200 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1---the-dye-house",
+      "name": "1 - The Dye House",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.28214937739919144,
+        "pitch": 0.1269218852616767,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9982690137859258,
+          "pitch": 0.020414508667869313,
+          "rotation": 0,
+          "target": "1-shop"
+        },
+        {
+          "yaw": -1.490752296547031,
+          "pitch": 0.021186512778678335,
+          "rotation": 0,
+          "target": "1-shop"
+        },
+        {
+          "yaw": -2.2102445320304405,
+          "pitch": -0.010223842195857813,
+          "rotation": 0,
+          "target": "1-shop"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.9881090787686411,
+          "pitch": -0.20185159542203834,
+          "title": "The Design Yard",
+          "text": "<span style=\"font-size: 13px;\">Information and details about the business and what they do.</span><div style=\"font-size: 13px;\"><br></div><div style=\"font-size: 13px;\">Opening Hours</div><div style=\"font-size: 13px;\">09:00 - 18:00 Mon - Sat</div><div style=\"font-size: 13px;\">12:00 - 16:00 Sun<br><div><br></div><div>Contact Information</div></div><div style=\"font-size: 13px;\">info@businessname.ext</div>"
+        },
+        {
+          "yaw": -0.6591437546370784,
+          "pitch": -0.1637350063248313,
+          "title": "Pavers",
+          "text": "<span style=\"font-size: 13px;\">Information and details about the business and what they do.</span><div style=\"font-size: 13px;\"><br></div><div style=\"font-size: 13px;\">Opening Hours</div><div style=\"font-size: 13px;\">09:00 - 18:00 Mon - Sat</div><div style=\"font-size: 13px;\">12:00 - 16:00 Sun<br><div><br></div><div>Contact Information</div></div><div style=\"font-size: 13px;\">info@businessname.ext</div>"
+        },
+        {
+          "yaw": -1.4896113322331335,
+          "pitch": -0.21423203325725204,
+          "title": "Cafe Bedeck",
+          "text": "<span style=\"font-size: 13px;\">Information and details about the business and what they do.</span><div style=\"font-size: 13px;\"><br></div><div style=\"font-size: 13px;\">Opening Hours</div><div style=\"font-size: 13px;\">09:00 - 18:00 Mon - Sat</div><div style=\"font-size: 13px;\">12:00 - 16:00 Sun<br><div><br></div><div>Contact Information</div></div><div style=\"font-size: 13px;\">info@businessname.ext</div>"
+        },
+        {
+          "yaw": -2.2443068334497234,
+          "pitch": -0.2072502969749639,
+          "title": "Maison",
+          "text": "<span style=\"font-size: 13px;\">Information and details about the business and what they do.</span><div style=\"font-size: 13px;\"><br></div><div style=\"font-size: 13px;\">Opening Hours</div><div style=\"font-size: 13px;\">09:00 - 18:00 Mon - Sat</div><div style=\"font-size: 13px;\">12:00 - 16:00 Sun<br><div><br></div><div>Contact Information</div></div><div style=\"font-size: 13px;\">info@businessname.ext</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-shop",
+      "name": "Shop",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -0.7634393897498608,
+        "pitch": 0.35003843355305087,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.14599313711618578,
+          "pitch": 0.22492088991776704,
+          "rotation": 0,
+          "target": "0-1---the-dye-house"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-shop",
+      "name": "Shop",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -0.7634393897498608,
+        "pitch": 0.35003843355305087,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.14599313711618578,
+          "pitch": 0.22492088991776704,
+          "rotation": 0,
+          "target": "0-1---the-dye-house"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-shop",
+      "name": "Shop",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -0.7634393897498608,
+        "pitch": 0.35003843355305087,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.14599313711618578,
+          "pitch": 0.22492088991776704,
+          "rotation": 0,
+          "target": "0-1---the-dye-house"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Commercial Tour",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
